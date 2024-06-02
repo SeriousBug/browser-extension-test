f4c7f1fb4c183bd87b02985aaa00259f6e68720c
Kaan Barmore-Genç <kaan@bgenc.net>
2024-06-02 15:54:41 +0000

    Use error boundary provider

--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -7,6 +7,7 @@ import { useTooltip } from "@src/lib/useTooltip";
 import { useToggle } from "@src/lib/useToggle";
 import { AnimatePresence, motion } from "framer-motion";
 import { SShadowError } from "@src/lib/error";
+import { ErrorBoundaryProvider } from "@src/lib/wrappers/ErrorBoundary";
 
 type PortalProps = { portal: HTMLElement | DocumentFragment };
 
@@ -18,13 +19,15 @@ export function Control({ portal }: PortalProps) {
 
   return (
     <div className="w-12 h-12 flex justify-center items-center">
-      <button
-        className="w-full h-full p-2 hover:bg-[#e8eaed12] rounded-full relative"
-        onClick={open}
-        {...bindTarget}
-      >
-        <LogoSmall aria-label="" className="w-full h-full" />
-      </button>
+      <ErrorBoundaryProvider>
+        <button
+          className="w-full h-full p-2 hover:bg-[#e8eaed12] rounded-full relative"
+          onClick={open}
+          {...bindTarget}
+        >
+          <LogoSmall aria-label="" className="w-full h-full" />
+        </button>
+      </ErrorBoundaryProvider>
       {createPortal(
         <div
           className={clsx(
@@ -46,38 +49,40 @@ const MODAL = "fixed left-0 top-0 bottom-0 right-0 z-20";
 
 function Modal({ isOpen, onClose }: { isOpen: boolean; onClose?: () => void }) {
   return (
-    <AnimatePresence>
-      {isOpen ? (
-        <>
-          <motion.div
-            initial={{
-              opacity: 0,
-            }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.2, ease: "easeInOut" }}
-            className={clsx(MODAL, "bg-black bg-opacity-50 backdrop-blur")}
-          />
-          <motion.div
-            initial={{ opacity: 0, y: 200 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: 200 }}
-            transition={{ duration: 0.2, ease: "easeInOut" }}
-            className={clsx(MODAL, "flex justify-center items-center")}
-            aria-hidden
-            onClick={onClose}
-          >
-            <div className="bg-white p-4 rounded-lg">
-              <div className="flex flex-row gap-4">
-                <h2 className="text-xl">Modal</h2>
-                <button onClick={onClose}>Close</button>
+    <ErrorBoundaryProvider>
+      <AnimatePresence>
+        {isOpen ? (
+          <>
+            <motion.div
+              initial={{
+                opacity: 0,
+              }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.2, ease: "easeInOut" }}
+              className={clsx(MODAL, "bg-black bg-opacity-50 backdrop-blur")}
+            />
+            <motion.div
+              initial={{ opacity: 0, y: 200 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: 200 }}
+              transition={{ duration: 0.2, ease: "easeInOut" }}
+              className={clsx(MODAL, "flex justify-center items-center")}
+              aria-hidden
+              onClick={onClose}
+            >
+              <div className="bg-white p-4 rounded-lg">
+                <div className="flex flex-row gap-4">
+                  <h2 className="text-xl">Modal</h2>
+                  <button onClick={onClose}>Close</button>
+                </div>
+                <p>Content</p>
               </div>
-              <p>Content</p>
-            </div>
-          </motion.div>
-        </>
-      ) : null}
-    </AnimatePresence>
+            </motion.div>
+          </>
+        ) : null}
+      </AnimatePresence>
+    </ErrorBoundaryProvider>
   );
 }
 
